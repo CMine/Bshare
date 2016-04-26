bf3a24bed1999f4492c4281869916b54d10c77e3
Cameron Mine <minec@rpi.edu>
2016-04-26 17:56:28 -0400

    Util is saved
    Can now save the util amount that is spent in one transaction given the
    address of the proposed “company” is found in the output section of the
    addresses in the block

--- Parsing.py
+++ Parsing.py
@@ -21,9 +21,9 @@ def balance(walletaddress):
 
 def overview(walletaddress):
     '''gets the general overview of the wallet while also setting the
-    total received btc and total sent btc'''
-    global total_received 
-    global total_sent     
+        total received btc and total sent btc'''
+    global total_received
+    global total_sent
     x = blockcypher.get_address_overview(walletaddress)
     total_received = convert_to_btc(x.get("total_received"))
     total_sent = convert_to_btc(x.get("total_sent"))
@@ -32,31 +32,51 @@ def overview(walletaddress):
 def increase_ult(value):
     '''increases the amount of btc spend on utilities'''
     global util_spent
-    util_spent += convert_to_btc(value)
+    util_spent += value
+
+def incoming():
+    print("I don't know if am walking on solid ground")
 
 def transations(walletaddress):
     fullchain = blockcypher.get_address_full(address = walletaddress)
     transations = fullchain.get("txs")
     for i in range(len(transations)):
-        print(transations[i], "\n")
+        pprint.pprint(transations[i])
         trans_value = convert_to_btc((transations[i]).get("total"))
-        print(trans_value, "\n")
-        outputs
-    
+        inputs = (transations[i]).get("inputs")
+        input_addr = any(d['addresses'] == [walletaddress] for d in inputs)
+        
+        outputs = (transations[i]).get("outputs")
+        output_addr = any(d['addresses'] == [walletaddress] for d in outputs)
+        
+        if (input_addr == True):
+            for i in range (len(util_addresses)):
+                for d in outputs:
+                    if(d['addresses'] == util_addresses[i]):
+                        value = d.get("value")
+                        print(trans_value)
+                        increase_ult(trans_value)
+        else:
+            print ("THAT BOY AIN'T RIGHT")
+        if (output_addr == True):
+            print("PROPANE AND PROPANE ACCESSORIES")
+        else:
+            print ("DANG IT BOBBY")
+
+
+
 
 if __name__ == '__main__':
     
+    name = input("First and Last Name: ")
     walletaddress = "1FXqE2ixnnSB1kvwbMtWma5xQ2bVbkSq3f"
     # input("enter address: ")
     print("The Current Balance is",balance(walletaddress), "btc")
     overview(walletaddress)
+    transations('1FXqE2ixnnSB1kvwbMtWma5xQ2bVbkSq3f')
     print ("Total Sent:",total_sent, "btc")
     print ("Total Recieved:",total_received, "btc")
-    
-    #x = blockcypher.get_address_full(address='1FXqE2ixnnSB1kvwbMtWma5xQ2bVbkSq3f')
-    #w = x.get("txs")
-    transations('1FXqE2ixnnSB1kvwbMtWma5xQ2bVbkSq3f')
-    #pprint.pprint(w)
-    #pprint.pprint(w[0].get("addresses"))
-   
-    
+    print ("Util:",util_spent, "btc")
+
+
+
