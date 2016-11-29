a772b79da32f175197ada0fb5e458a5c5648e6c9
Cameron Mine <minec@rpi.edu>
2016-11-29 16:34:12 -0500

    Completed Cards
    Pulls info from the database which is continuously updated from a query
    on the user’s wallet address. Cards available: total_recieved,
    total_sent, final_balance, balance

--- Site_Bscore/client/pages/Dashboard.js
+++ Site_Bscore/client/pages/Dashboard.js
@@ -12,3 +12,20 @@ Template.Dashboard.helpers({
 		return Wallet.findOne();
 	}
 });
+
+Template.card.helpers({
+	wallet (){
+		return Wallet.findOne();
+	},
+	objlist (){
+		var result = [];
+		var obj = Wallet.findOne();
+		for (var key in obj){
+			if(key != 'address' && key != "unconfirmed_balance" && key != "n_tx"
+			&& key !="unconfirmed_n_tx" && key != "final_n_tx" && key != "_id"){
+				result.push({name:key,value:obj[key]});
+			}
+		}
+    return result;
+	}
+});
